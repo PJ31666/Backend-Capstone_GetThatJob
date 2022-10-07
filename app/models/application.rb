f1af7fb799d6f13846642101354340b2178c5eda
Dik Solis <dicksolisromero@gmail.com>
2022-10-07 11:23:26 -0500

    user controller

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,7 +2,7 @@ class Application < ApplicationRecord
   # ASOCIACIONES
   belongs_to :user
   belongs_to :job
-  # has_one_attached :new_cv
+  has_one_attached :new_cv
 
   # VALIDACIONES
   validates :new_cv, :professional_experience, :professional_interesed, presence: true
